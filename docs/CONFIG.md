ff4fa247f7ce61e16c5390c9a7c4ba9d1e2a0c7f
Jens Törnell <jens.tornell@gmail.com>
2018-07-25 21:59:26 +0200

    Init

--- docs/CONFIG.md
+++
@@ -1,9 +0,0 @@
-# Config
-
-## Field key
-
-If you want to use another field name than `schedule`, you need to add an option for it.
-
-```php
-c::set( 'plugin.scheduled.pages.field.key', 'schedule' ); // `schedule` is default
-```
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+Kirby::plugin('jenstornell/scheduled-pages', [
+    'hooks' => [
+        'route:after' => function ($route, $path, $method, $result) {
+            if(!isset($result) || !property_exists($result, 'content')) return;
+
+            $timestamp = strtotime($result->content()->scheduled());
+            if($timestamp != 0 && $timestamp > time()) {
+                header('HTTP/1.0 404 Not Found');
+                echo page((string)site()->errorPage())->render();
+                die;
+            }
+        }
+    ],
+    'pageMethods' => [
+        'scheduled' => function() {
+            $timestamp = strtotime($this->content()->scheduled());
+            if($timestamp !=0 && $timestamp > time())
+                return true;
+            return false;
+        }
+    ],
+    'pagesMethods' => [
+        'scheduled' => function($match = true) {
+            if($match)
+                return $this->filterBy('scheduled', true);
+            return $this->filterBy('scheduled', '!=', true);
+        }
+    ],
+]);
